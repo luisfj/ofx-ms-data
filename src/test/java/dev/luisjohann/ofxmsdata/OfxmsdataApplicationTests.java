89450fc3118373cc76fa1f75ce4c1d6705bf5e19
luis <luisfj_pr@hotmail.com>
2024-07-30 20:02:04 +0000

    Update for CI CD pass

--- src/test/java/dev/luisjohann/ofxmsdata/OfxmsdataApplicationTests.java
+++ src/test/java/dev/luisjohann/ofxmsdata/OfxmsdataApplicationTests.java
@@ -1,13 +1,38 @@
 package dev.luisjohann.ofxmsdata;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+// @ExtendWith(SpringExtension.class)
+// We create a `@SpringBootTest`, starting an actual server on a `RANDOM_PORT`
+// @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+// @AutoConfigureWebTestClient
 @SpringBootTest
+@ActiveProfiles("dev")
 class OfxmsdataApplicationTests {
 
+	// @Autowired
+	// private WebTestClient webTestClient;
+
+	// @Test
+	// public void testHello() {
+	// webTestClient
+	// // Create a GET request to test an endpoint
+	// .get().uri("/hello")
+	// .accept(MediaType.APPLICATION_JSON)
+	// .exchange()
+	// // and use the dedicated DSL to test assertions against the response
+	// .expectStatus().isOk()
+	// .expectBody(Greeting.class).value(greeting -> {
+	// assertThat(greeting.getMessage()).isEqualTo("Hello, Spring!");
+	// });
+	// }
+
 	@Test
-	void contextLoads() {
+	void teste() {
+		Assertions.assertTrue(Boolean.TRUE);
 	}
 
 }
